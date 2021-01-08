5c43e1fb7e2edbc75be3760b843a4accc8eb9142
Sebastian <github_com@planetcat.de>
2021-01-08 01:21:50 +0100

    Added script for dropdown and second keyword set

---
+++ dropdown.js
@@ -0,0 +1,40 @@
+var keyword_1 = "corona"; //Dont use this. For internal use only. External please go to notify_keyword
+var keyword_2 = "keines"
+
+document.querySelector("#keyword1").onchange = function () {
+   let msg = document.querySelector("#keyword1").value;
+   if (keyword_2 = msg) {
+     selectItemByValue(document.getElementById("keyword2"), "keines");
+   }
+   keyword_1 = msg;
+   notify_keyword_1_changed(msg);
+}
+document.querySelector("#keyword2").onchange = function () {
+   let msg = document.querySelector("#keyword2").value;
+   if (keyword_1 == msg) {
+     selectItemByValue(document.getElementById("keyword2"), "keines");
+     alert("Keyword schon als Keyword 1 gewählt. Bitte anderes aussuchen.");
+   } else {
+     keyword_2 = msg;
+     notify_keyword_2_changed(msg);
+   }
+}
+
+//WANT THE CURRENT KEYWORD? PUT YOUR FUNCTION IN HERE
+function notify_keyword_1_changed() {
+
+}
+function notify_keyword_2_changed() {
+
+}
+
+function selectItemByValue(elmnt, value){
+
+  for(var i=0; i < elmnt.options.length; i++)
+  {
+    if(elmnt.options[i].value === value) {
+      elmnt.selectedIndex = i;
+      break;
+    }
+  }
+}
