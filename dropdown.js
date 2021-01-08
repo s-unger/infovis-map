e8e7152bfe46a97181c8532bd01b8f8b60237e78
Sebastian <github_com@planetcat.de>
2021-01-08 01:29:10 +0100

    articles now react to keyword change

--- dropdown.js
+++ dropdown.js
@@ -5,6 +5,7 @@ document.querySelector("#keyword1").onchange = function () {
    let msg = document.querySelector("#keyword1").value;
    if (keyword_2 = msg) {
      selectItemByValue(document.getElementById("keyword2"), "keines");
+     notify_keyword_2_changed("keines");
    }
    keyword_1 = msg;
    notify_keyword_1_changed(msg);
@@ -13,6 +14,7 @@ document.querySelector("#keyword2").onchange = function () {
    let msg = document.querySelector("#keyword2").value;
    if (keyword_1 == msg) {
      selectItemByValue(document.getElementById("keyword2"), "keines");
+     notify_keyword_2_changed("keines");
      alert("Keyword schon als Keyword 1 gewählt. Bitte anderes aussuchen.");
    } else {
      keyword_2 = msg;
@@ -21,10 +23,10 @@ document.querySelector("#keyword2").onchange = function () {
 }
 
 //WANT THE CURRENT KEYWORD? PUT YOUR FUNCTION IN HERE
-function notify_keyword_1_changed() {
-
+function notify_keyword_1_changed(keyword) {
+  updateArticleKeyword(keyword);
 }
-function notify_keyword_2_changed() {
+function notify_keyword_2_changed(keyword) {
 
 }
 
