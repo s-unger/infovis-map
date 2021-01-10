896b8d744f9d3f7a3b3cb95beee2d8b49cf4e186
Sebastian <github_com@planetcat.de>
2021-01-10 18:50:35 +0100

    tagcloud is now responsive

--- wordcloud.js
+++ wordcloud.js
@@ -1,9 +1,10 @@
 var wordcloud_files = [];
 draw_wordcloud("CW 00/2019");
+var wordcloud_last_calendar_week = -1;
+var wordcloud_effect_running = false;
 
 
 function draw_wordcloud(weekandyear_text) {
-  document.getElementById("tagcloud").innerHTML = "<h2>Meistgesucht in dieser Woche</h2>"
   if(wordcloud_files.length == 0) {
     get_all_data(weekandyear_text);
   } else {
@@ -14,73 +15,77 @@ function draw_wordcloud(weekandyear_text) {
     if (year == 2020) {
       calendarweek = calendarweek+52;
     }
-    
-    // List of words
-    var myWords = [{word: "alkohol", size: wordcloud_files[0][calendarweek].value}, {word: "attila hildmann", size: wordcloud_files[1][calendarweek].value}, {word: "bananenbrot", size: wordcloud_files[2][calendarweek].value}, {word: "baumarkt", size: wordcloud_files[3][calendarweek].value}, {word: "BER", size: wordcloud_files[4][calendarweek].value}, {word: "bill gates", size: wordcloud_files[5][calendarweek].value}, {word: "corona", size: wordcloud_files[5][calendarweek].value}, {word: "desinfektionsmittel", size: wordcloud_files[5][calendarweek].value}, {word: "drosten", size: wordcloud_files[5][calendarweek].value}, {word: "homeworkout", size: wordcloud_files[5][calendarweek].value}, {word: "klopapier", size: wordcloud_files[5][calendarweek].value}, {word: "kneipentour", size: wordcloud_files[5][calendarweek].value}, {word: "maske", size: wordcloud_files[5][calendarweek].value}, {word: "merkel", size: wordcloud_files[5][calendarweek].value}, {word: "netflix", size: wordcloud_files[5][calendarweek].value}, {word: "oktoberfest", size: wordcloud_files[5][calendarweek].value}, {word: "onlinesemester", size: wordcloud_files[5][calendarweek].value}, {word: "querdenken", size: wordcloud_files[5][calendarweek].value}, {word: "söder", size: wordcloud_files[5][calendarweek].value}, {word: "zoom", size: wordcloud_files[5][calendarweek].value}]
-    
-    for (var i = 0; i<20; i++) {
-      myWords[i].size = 15+(myWords[i].size*0.8); //Kompressionsfaktor, es geht nur darum, dass die Peaks der Wörter hier sichtbar sind, nicht um akurate Darstellung.
-    }
+    if (calendarweek != wordcloud_last_calendar_week) {
+      wordcloud_last_calendar_week = calendarweek;
+      document.getElementById("tagcloud").innerHTML = "<h2>Meistgesucht in dieser Woche</h2>";
 
-    // set the dimensions and margins of the graph
-    var marginWords = {top: 0, right: 0, bottom: 0, left: 0},
-        widthWords = 550 - marginWords.left - marginWords.right,
-        heightWords = 300 - marginWords.top - marginWords.bottom;
+      
+      // List of words
+      var myWords = [{word: "alkohol", size: wordcloud_files[0][calendarweek].value}, {word: "attila hildmann", size: wordcloud_files[1][calendarweek].value}, {word: "bananenbrot", size: wordcloud_files[2][calendarweek].value}, {word: "baumarkt", size: wordcloud_files[3][calendarweek].value}, {word: "BER", size: wordcloud_files[4][calendarweek].value}, {word: "bill gates", size: wordcloud_files[5][calendarweek].value}, {word: "corona", size: wordcloud_files[5][calendarweek].value}, {word: "desinfektionsmittel", size: wordcloud_files[5][calendarweek].value}, {word: "drosten", size: wordcloud_files[5][calendarweek].value}, {word: "homeworkout", size: wordcloud_files[5][calendarweek].value}, {word: "klopapier", size: wordcloud_files[5][calendarweek].value}, {word: "kneipentour", size: wordcloud_files[5][calendarweek].value}, {word: "maske", size: wordcloud_files[5][calendarweek].value}, {word: "merkel", size: wordcloud_files[5][calendarweek].value}, {word: "netflix", size: wordcloud_files[5][calendarweek].value}, {word: "oktoberfest", size: wordcloud_files[5][calendarweek].value}, {word: "onlinesemester", size: wordcloud_files[5][calendarweek].value}, {word: "querdenken", size: wordcloud_files[5][calendarweek].value}, {word: "söder", size: wordcloud_files[5][calendarweek].value}, {word: "zoom", size: wordcloud_files[5][calendarweek].value}]
+      
+      for (var i = 0; i<20; i++) {
+        myWords[i].size = 15+(myWords[i].size*0.8); //Kompressionsfaktor, es geht nur darum, dass die Peaks der Wörter hier sichtbar sind, nicht um akurate Darstellung.
+      }
 
-    // append the svg object to the body of the page
-    var svg2 = d3.select("#tagcloud").append("svg")
-        .attr("width", widthWords + marginWords.left + marginWords.right)
-        .attr("height", heightWords + marginWords.top + marginWords.bottom)
-      .append("g")
-        .attr("transform",
-        "translate(" + marginWords.left + "," + marginWords.top + ")");
-        
-     var borderPath = svg2.append("rect")
-     .attr("x", 0)
-     .attr("y", 0)
-     .attr("height", heightWords)
-     .attr("width", widthWords)
-     .style("stroke", "black")
-     .style("fill", "none")
-     .style("stroke-width", 2);
+      // set the dimensions and margins of the graph
+      var marginWords = {top: 0, right: 0, bottom: 0, left: 0},
+          widthWords = 550 - marginWords.left - marginWords.right,
+          heightWords = 300 - marginWords.top - marginWords.bottom;
 
-    // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
-    // Wordcloud features that are different from one word to the other must be here
-    var layout = d3.layout.cloud()
-      .size([widthWords, heightWords])
-      .words(myWords.map(function(d) { return {text: d.word, size:d.size}; }))
-      .padding(5)        //space between words
-      .rotate(function() { return 0; })
-      .fontSize(function(d) { return d.size; })      // font size of words
-      .on("end", draw);
-    layout.start();
-
-    // This function takes the output of 'layout' above and draw the words
-    // Wordcloud features that are THE SAME from one word to the other can be here
-    function draw(words) {
-      svg2
+      // append the svg object to the body of the page
+      var svg2 = d3.select("#tagcloud").append("svg")
+          .attr("viewBox", `0 0 550 300`)
         .append("g")
-          .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-          .selectAll("text")
-      .data(words)
-          .enter().append("text")
-      .on("click", function (d, i){
-          select_keyword_1(d.text);
-      })
-      .on("mouseover", function(d) {   
-        d3.select(this).style("cursor", "pointer");
-      })                  
-      .on("mouseout", function(d) {       
-        d3.select(this).style("cursor", "default");
-      })
-      .style("font-size", function(d) { return d.size; })
-      .style("fill", "#000000")
-      .attr("text-anchor", "middle")
-      .style("font-family", "Impact")
-      .attr("transform", function(d) {
-        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-      })
-      .text(function(d) { return d.text; });
+          .attr("transform",
+          "translate(" + marginWords.left + "," + marginWords.top + ")");
+          
+       var borderPath = svg2.append("rect")
+       .attr("x", 0)
+       .attr("y", 0)
+       .attr("height", heightWords)
+       .attr("width", widthWords)
+       .style("stroke", "black")
+       .style("fill", "none")
+       .style("stroke-width", 2);
+
+      // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+      // Wordcloud features that are different from one word to the other must be here
+      var layout = d3.layout.cloud()
+        .size([widthWords, heightWords])
+        .words(myWords.map(function(d) { return {text: d.word, size:d.size}; }))
+        .padding(5)        //space between words
+        .rotate(function() { return 0; })
+        .fontSize(function(d) { return d.size; })      // font size of words
+        .on("end", draw);
+      layout.start();
+
+      // This function takes the output of 'layout' above and draw the words
+      // Wordcloud features that are THE SAME from one word to the other can be here
+      function draw(words) {
+        svg2
+          .append("g")
+            .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+            .selectAll("text")
+        .data(words)
+            .enter().append("text")
+        .on("click", function (d, i){
+            select_keyword_1(d.text);
+        })
+        .on("mouseover", function(d) {   
+          d3.select(this).style("cursor", "pointer");
+        })                  
+        .on("mouseout", function(d) {       
+          d3.select(this).style("cursor", "default");
+        })
+        .style("font-size", function(d) { return d.size; })
+        .style("fill", "#000000")
+        .attr("text-anchor", "middle")
+        .style("font-family", "Impact")
+        .attr("transform", function(d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+      }
     }
   }
 }
